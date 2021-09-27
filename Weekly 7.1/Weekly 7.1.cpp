cfc0d350f71eac0b578da5fdd332b88106e0b82e
SAKSOPON PIMKAEW <64010942@KMITL.AC.TH>
2021-09-27 17:08:24 +0700

    SS1

--- Weekly 7.1/Weekly 7.1.cpp
+++ Weekly 7.1/Weekly 7.1.cpp
@@ -1,9 +1,14 @@
-
-
-#include <iostream>
+#include <stdio.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double a, b;
+    scanf_s("%lf %lf", &a, &b);
+    a = a - b;
+    b = a + b;
+    a = b - a;
+    printf("A= %lf\n", a);
+    printf("B= %lf\n", b);
+    return 0;
 }
 
